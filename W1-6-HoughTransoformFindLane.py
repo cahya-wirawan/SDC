3b7552dc657261509c8a3796e4595d4397cacc31
Cahya Wirawan <cahya.wirawan@gmail.com>
2017-10-12 22:06:19 +0200

    Test with new images

--- W1-6-HoughTransoformFindLane.py
+++ W1-6-HoughTransoformFindLane.py
@@ -5,7 +5,7 @@ import cv2
 
 
 # Read in and grayscale the image
-image = mpimg.imread('../Term1/Finding Lane Line/exit-ramp.jpg')
+image = mpimg.imread('test_images/solidWhiteRight.jpg')
 gray = cv2.cvtColor(image,cv2.COLOR_RGB2GRAY)
 
 # Define a kernel size and apply Gaussian smoothing
@@ -14,7 +14,7 @@ blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
 
 # Define our parameters for Canny and apply
 low_threshold = 50
-high_threshold = 150
+high_threshold = 220
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 
 # Next we'll create a masked edges image using cv2.fillPoly()
@@ -23,17 +23,17 @@ ignore_mask_color = 255
 
 # This time we are defining a four sided polygon to mask
 imshape = image.shape
-vertices = np.array([[(0,imshape[0]),(450, 290), (490, 290), (imshape[1],imshape[0])]], dtype=np.int32)
+vertices = np.array([[(0,imshape[0]),(450, 310), (490, 310), (imshape[1],imshape[0])]], dtype=np.int32)
 cv2.fillPoly(mask, vertices, ignore_mask_color)
 masked_edges = cv2.bitwise_and(edges, mask)
 
 # Define the Hough transform parameters
 # Make a blank the same size as our image to draw on
-rho = 2 # distance resolution in pixels of the Hough grid
-theta = np.pi/180 # angular resolution in radians of the Hough grid
-threshold = 15   # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 40 #minimum number of pixels making up a line
-max_line_gap = 20    # maximum gap in pixels between connectable line segments
+rho = 2 # #2 distance resolution in pixels of the Hough grid
+theta = np.pi/180 # #120 angular resolution in radians of the Hough grid
+threshold = 15   # #15 minimum number of votes (intersections in Hough grid cell)
+min_line_length = 40 # #40 minimum number of pixels making up a line
+max_line_gap = 20    # #20 maximum gap in pixels between connectable line segments
 line_image = np.copy(image)*0 # creating a blank to draw lines on
 
 # Run Hough on edge detected image
